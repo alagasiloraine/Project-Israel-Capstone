b6ef40dcc7fea12ef245c9a3210e4bbf60e08b30
PerjesCy <perjescykean356@gmail.com>
2025-05-28 20:22:51 +0800

    mga notification at sms(nakacomment muna para di maubos free sms)

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ from apscheduler.triggers.cron import CronTrigger
 
 # Custom module imports
 from app.services.firebase_service import firebase_admin
-from app.routers import crop_router, auth_router, forecast_router, sensor_data, notif, motor_status, water_scheduling_router
+from app.routers import crop_router, auth_router, forecast_router, sensor_data, notif, motor_status, water_scheduling_router, sms
 from app.ml.weather_ml.forecast.forecast import main as run_forecast
 from app.ml.weather_ml.forecast.get_dataset import main as update_dataset
 from app.services.backend_ip import save_backend_ip
@@ -47,6 +47,7 @@ app.include_router(sensor_data.router)
 app.include_router(notif.router)
 app.include_router(motor_status.router)
 app.include_router(water_scheduling_router.router)
+app.include_router(sms.router)
 
 
 # ======== MIDDLEWARE FOR WS =========
