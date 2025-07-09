5a3190e02578b2122b7203950ae5b346d5abae98
CyKean <perjescykean356@gmail.com>
2025-07-10 05:18:40 +0800

    Remove ignored files from tracking

--- backend/test_serial.py
+++
@@ -1,8 +0,0 @@
-import serial
-
-print("✅ pyserial is loaded")
-print(serial.__file__)
-print(dir(serial))
-print("💡 Creating Serial instance...")
-ser = serial.Serial('COM4', 9600)  # Replace with actual COM port
-print("✅ Serial connection established")
